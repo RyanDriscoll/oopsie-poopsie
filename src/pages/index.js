77d3a4e7ba62370d5ae63e6faaa90220a2bb849e
ryan <driscollrp@gmail.com>
2020-05-10 15:58:31 -0600

    added dropdown for selecting time limit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,10 @@ import Label from "reactstrap/lib/Label"
 import Input from "reactstrap/lib/Input"
 import InputGroup from "reactstrap/lib/InputGroup"
 import InputGroupAddon from "reactstrap/lib/InputGroupAddon"
+import Dropdown from "reactstrap/lib/Dropdown"
+import DropdownItem from "reactstrap/lib/DropdownItem"
+import DropdownMenu from "reactstrap/lib/DropdownMenu"
+import DropdownToggle from "reactstrap/lib/DropdownToggle"
 import Row from "reactstrap/lib/Row"
 import { withRouter } from "next/router"
 import { absoluteUrl } from "../utils/helpers"
@@ -18,6 +22,8 @@ import Col from "reactstrap/lib/Col"
 import CombinedContext from "../context/CombinedContext"
 import classnames from "classnames"
 
+const timeLimitOptions = ["none", "90", "60", "30", "10"]
+
 const CreateGame = ({ origin, router }) => {
   const [name, setName] = useState("")
   const [game, setGame] = useState("")
@@ -27,8 +33,10 @@ const CreateGame = ({ origin, router }) => {
   const [copySuccess, setCopySuccess] = useState("")
   const [dirty, setDirty] = useState(false)
   const [bidPoints, setBidPoints] = useState(false)
+  const [timeLimit, setTimeLimit] = useState("")
   const [numCards, setNumCards] = useState(5)
   const [create, setCreate] = useState(true)
+  const [dropdownOpen, setDropdownOpen] = useState(false)
 
   const gameUrlRef = useRef(null)
 
@@ -41,6 +49,10 @@ const CreateGame = ({ origin, router }) => {
       setState({ mounted: false })
     }
   }, [])
+
+  const toggle = () => {
+    setDropdownOpen(prevState => !prevState)
+  }
 
   const handleChange = e => {
     const { name, value } = e.target
@@ -59,11 +71,18 @@ const CreateGame = ({ origin, router }) => {
     }
   }
 
-  const handleToggle = inc => {
+  const handleNumCards = inc => {
     const newNumCards = inc ? numCards + 1 : numCards - 1
     if (newNumCards <= 10 && newNumCards >= 1) {
       setNumCards(newNumCards)
     }
+  }
+
+  const handleDropDown = (e, val) => {
+    setTimeLimit(val || null)
+    setTimeout(() => {
+      setDropdownOpen(false)
+    }, 10)
   }
 
   const initializeGame = async () => {
@@ -74,7 +93,8 @@ const CreateGame = ({ origin, router }) => {
         name,
         numCards,
         bidPoints,
-        dirty
+        dirty,
+        timeLimit: timeLimit ? Number(timeLimit) : null
       }
       const response = await newGame(body)
       if (response.ok) {
@@ -205,7 +225,7 @@ const CreateGame = ({ origin, router }) => {
                       <InputGroupAddon addonType="prepend">
                         <Button
                           color="danger"
-                          onClick={e => handleToggle(false, e.target.value)}
+                          onClick={() => handleNumCards(false)}
                         >
                           -
                         </Button>
@@ -222,13 +242,40 @@ const CreateGame = ({ origin, router }) => {
                       <InputGroupAddon addonType="append">
                         <Button
                           color="success"
-                          onClick={e => handleToggle(true, e.target.value)}
+                          onClick={() => handleNumCards(true)}
                         >
                           +
                         </Button>
                       </InputGroupAddon>
                     </InputGroup>
                   </FormGroup>
+                </Col>
+
+                <Col xs="12" className="p-0 mb-3">
+                  <Label>Time limit</Label>
+
+                  <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+                    <DropdownToggle caret>
+                      {timeLimit ? `${timeLimit} seconds` : "None"}
+                    </DropdownToggle>
+                    <DropdownMenu flip={true} style={{ borderRadius: 0 }}>
+                      <DropdownItem onClick={e => handleDropDown(e)}>
+                        none
+                      </DropdownItem>
+                      <DropdownItem onClick={e => handleDropDown(e, 90)}>
+                        90 seconds
+                      </DropdownItem>
+                      <DropdownItem onClick={e => handleDropDown(e, 60)}>
+                        60 seconds
+                      </DropdownItem>
+                      <DropdownItem onClick={e => handleDropDown(e, 30)}>
+                        30 seconds
+                      </DropdownItem>
+                      <DropdownItem onClick={e => handleDropDown(e, 10)}>
+                        10 seconds
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </Dropdown>
                 </Col>
 
                 <FormGroup check>
